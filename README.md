b286becb3b0ed38af9725a93cdeb440893747c2f
AndresCayubuntu <cristiandres4444@hotmail.com>
2024-06-07 09:14:41 -0500

    add: pandas/fastAPI/uvicorn

--- README.md
+++ README.md
@@ -9,3 +9,14 @@ Param correr el juego debes seguir las siguientes instrucciones
 cd game
 python3 main.py
 ```
+
+# App Project
+
+```
+git clone
+cd app
+python3 -m venv env 
+source env/bin/activate
+pip3 install -r requirements.txt
+python3 main.py
+```
--- app/main.py
+++ app/main.py
@@ -1,22 +1,28 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 
+# def run():
+#     country = input('Selecciona un pais =>').title()
+#     data = read_csv.read_csv('data.csv')
+#     data = list(filter(lambda item: item['Continent'] == 'South America', data))
+#     country_data = utils.population_by_country(data, country)
+#     lables, values = utils.get_population(country_data[0])
+#     charts.generate_bar_chart(country,lables, values)
+
+#     lables, values = utils.get_population_percentage(data)
+#     charts.generate_pie_chart(lables, values)
+
+# Con pandas
 def run():
-    # names = {'Nicolas', 'Miguel', 'Juan', 'Nicolas'}
-    # print(names)
-    country = input('Selecciona un pais =>').title()
-    data = read_csv.read_csv('data.csv')
-    data = list(filter(lambda item: item['Continent'] == 'South America', data))
-    country_data = utils.population_by_country(data, country)
-    lables, values = utils.get_population(country_data[0])
-    # print(lables, values)
-    charts.generate_bar_chart(country,lables, values)
+    df = pd.read_csv('data.csv')
+    df = df[df['Continent'] == 'Africa']
 
-    lables, values = utils.get_population_percentage(data)
-    charts.generate_pie_chart(lables, values)
-
+    countries = df['Country/Territory'].values
+    percentages = df['World Population Percentage'].values
+    charts.generate_pie_chart(countries, percentages)
 
 if __name__ == '__main__':
     run()
